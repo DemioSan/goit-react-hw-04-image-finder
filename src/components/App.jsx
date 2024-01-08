917c562a16a0170277e2820245a7f1e063b6549e
DemioSan <132845079+DemioSan@users.noreply.github.com>
2024-01-08 18:45:36 +0100

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { AppDiv } from './App.styled';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -7,104 +7,87 @@ import { Button } from './Button/Button';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    items: [],
-    querry: '',
-    page: 1,
-    selectedImage: '',
-    showBtn: false,
-    showLoad: false,
-    showModal: false,
-  };
+export const App = () => {
+  const [items, setItems] = useState([]);
+  const [querry, setQuerry] = useState('');
+  const [page, setPage] = useState(1);
+  const [selectedImage, setSelectedImage] = useState('');
+  const [showBtn, setShowBtn] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showLoad, setShowLoad] = useState(false);
 
-  componentDidUpdate(_, prevState) {
-    if (
-      (this.state.querry !== prevState.querry && this.state.page === 1) ||
-      (this.state.querry === prevState.querry &&
-        this.state.page !== prevState.page)
-    ) {
-      if (this.state.page === 1) {
-        this.searchService();
-      } else {
-        this.loadMoreService();
-      }
-    }
-  }
-
-  searchService = async () => {
-    this.setState({ showLoad: true, showBtn: false });
-
-    const q = this.state.querry;
-
-    try {
-      const imags = await fetchImages({ q });
-      this.setState({ items: imags.hits, showBtn: imags.hits.length >= 15 });
-    } catch (err) {
-      console.log(err);
-    } finally {
-      this.setState({ showLoad: false });
-    }
-  };
-
-  handleSearch = e => {
+  const handleSearch = e => {
     e.preventDefault();
 
     const q = e.target.elements.input.value;
 
-    this.setState({
-      querry: q,
-      page: 1,
-    });
+    setQuerry(q);
+    setPage(1);
   };
 
-  loadMoreService = async () => {
-    this.setState({ showLoad: true, showBtn: false });
+  const handleLoadMore = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const handleOpenModal = imageUrl => {
+    setShowModal(true);
+    setSelectedImage(imageUrl);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedImage('');
+  };
+
+  const searchService = useCallback(async () => {
+    setShowLoad(true);
+    setShowBtn(false);
+
+    const q = querry;
+
+    try {
+      const imags = await fetchImages({ q });
+      setItems(imags.hits);
+      setShowBtn(imags.hits.length >= 15);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setShowLoad(false);
+    }
+  }, [querry, page]);
+
+  const loadMoreService = useCallback(async () => {
+    setShowLoad(true);
+    setShowBtn(false);
+
     try {
       const imags = await fetchImages({
-        q: this.state.querry,
-        page: this.state.page,
+        q: querry,
+        page: page,
       });
-      this.setState(prev => ({
-        items: [...prev.items, ...imags.hits],
-        showBtn: imags.hits.length >= 15,
-      }));
+      setItems(prev => [...prev, ...imags.hits]);
+      setShowBtn(imags.hits.length >= 15);
     } catch (err) {
       console.log('Error in loadMoreService:', err);
     } finally {
-      this.setState({ showLoad: false });
+      setShowLoad(false);
     }
-  };
+  }, [page]);
 
-  handleLoadMore = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
-  };
+  useEffect(() => {
+    if (querry && page === 1) searchService();
+    if (page !== 1) loadMoreService();
+  }, [querry, page, searchService, loadMoreService]);
 
-  handleOpenModal = imageUrl => {
-    this.setState({ showModal: true, selectedImage: imageUrl });
-  };
-
-  handleCloseModal = () => {
-    this.setState({ showModal: false, selectedImage: '' });
-  };
-
-  render() {
-    return (
-      <AppDiv>
-        <Searchbar onSubmit={this.handleSearch} />
-        <ImageGallery
-          items={this.state.items}
-          onOpenModal={this.handleOpenModal}
-        />
-        {this.state.showLoad && <Loader />}
-        {this.state.showBtn && <Button onClick={this.handleLoadMore} />}
-        {this.state.showModal && (
-          <Modal
-            imageUrl={this.state.selectedImage}
-            onClose={this.handleCloseModal}
-          />
-        )}
-      </AppDiv>
-    );
-  }
-}
+  return (
+    <AppDiv>
+      <Searchbar onSubmit={handleSearch} />
+      <ImageGallery items={items} onOpenModal={handleOpenModal} />
+      {showLoad && <Loader />}
+      {showBtn && <Button onClick={handleLoadMore} />}
+      {showModal && (
+        <Modal imageUrl={selectedImage} onClose={handleCloseModal} />
+      )}
+    </AppDiv>
+  );
+};
